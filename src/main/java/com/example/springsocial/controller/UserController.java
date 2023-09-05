67327d84af26d747602b2dd8a9e97ec75048a04d
DAVIDLEONE2001 <david.leone.2001@gmail.com>
2023-09-05 14:12:04 +0200

    First commit

---
+++ src/main/java/com/example/springsocial/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.springsocial.controller;
+
+import com.example.springsocial.exception.ResourceNotFoundException;
+import com.example.springsocial.model.User;
+import com.example.springsocial.repository.UserRepository;
+import com.example.springsocial.security.CurrentUser;
+import com.example.springsocial.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        System.out.println(userRepository.findById(userPrincipal.getId()));
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+}
